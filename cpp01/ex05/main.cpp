8b1ad8b06f4d34acca68ab547953294e7c38367b
Manuel Martin Lopez <manuelmartinlopez02@gmail.com>
2021-10-15 21:54:51 +0200

    cpp01

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Karen.hpp"
+
+int main (void) {
+    Karen   karen;
+    karen.complain("1");
+    karen.complain("2");
+    karen.complain("3");
+    karen.complain("4");
+
+
+    karen.complain("-1");
+    karen.complain("hola");
+    karen.complain("83");
+    return(0);
+}

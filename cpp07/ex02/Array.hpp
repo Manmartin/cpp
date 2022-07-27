258a4107cc354e9e8617529f7c14cabeb5e91977
manmartin <manuelmartinlopez02@gmail.com>
2022-07-27 23:28:43 +0200

    cpp07 ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,42 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+
+template < typename T >
+class Array {
+	private:
+		unsigned int _size;
+		T *_arr;
+				
+	public:
+		Array<T>( void ) : _size(0), _arr(new T[0]())   {}
+		Array<T>( Array<T> const &ref ) { *this = ref; } 
+		Array<T>( unsigned int n ) : _size(n), _arr(new T[_size]()) {}
+		~Array<T>() {delete [] this->_arr;}
+		
+		Array<T>	&operator=( Array<T> const &ref ) {
+			T *tmp = ref._size > 0 ? new T[ref._size]() : new T[0]();
+			this->_size = ref._size;
+			for (unsigned int i = 0; i < this->_size; i++)
+				tmp[i] = ref._arr[i];
+			delete [] this->_arr;
+			this->_arr = tmp;
+			return *this;
+		}
+		T	&operator[]( const unsigned int i ) {
+			if (i >= this->_size)
+				throw std::exception();
+			else
+				return this->_arr[i];
+		}
+		T const &operator[]( const unsigned int i ) const {
+			if (i >= this->_size)
+				throw std::exception();
+			else
+				return this->_arr[i];
+		}
+		unsigned int	size( void ) const {return this->_size;}
+};
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,141 @@
+#include <Array.hpp>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+
+typedef struct {
+	double i;
+	double x;
+} nc;
+
+int main(void) {
+
+	{
+		unsigned int size;
+		std::cout << BLUE << "Int\n\n";
+		std::cout << GREEN << "Creating 2 Array<int> (Empty & size = 5)\n\n" << RESET;
+		Array<int> arrInt; 
+		Array<int> arrInt2(5);
+		std::cout << "size = " << arrInt.size() << '\n';
+		std::cout << "size = " << arrInt2.size() << '\n';
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator to change and print array elements\n\n" << RESET;
+		arrInt = arrInt2;
+		arrInt2 = arrInt;
+		size = arrInt2.size();
+		for (unsigned int i = 0; i < size; i++) {
+			arrInt2[i] = i;
+			std::cout << arrInt2[i] << '\n';
+		}
+		std::cout << '\n';
+		size = arrInt.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << arrInt[i] << '\n';
+		}
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator in a const Array<int>\n\n" << RESET;
+		Array<int> const constArr(5);
+		size = constArr.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << constArr[i] << '\n';
+		}
+
+		std::cout << '\n';
+		std::cout << GREEN << "trying bad index in Array<int>\n\n" << RESET;
+		try {
+			size = arrInt.size();
+			arrInt[size];
+		} catch (std::exception &e) {
+			std::cout << RED << "Bad index\n" << RESET;
+		}
+	}
+	{
+		unsigned int size;
+		std::cout << BLUE << "String\n\n";
+		std::cout << GREEN << "Creating 2 Array<std::string> (Empty & size = 5)\n\n" << RESET;
+		Array<std::string> arrStr; 
+		Array<std::string> arrStr2(5);
+		std::cout << "size = " << arrStr.size() << '\n';
+		std::cout << "size = " << arrStr2.size() << '\n';
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator to change and print array elements\n\n" << RESET;
+		arrStr = arrStr2;
+		arrStr2 = arrStr;
+		size = arrStr2.size();
+		for (unsigned int i = 0; i < size; i++) {
+			arrStr2[i] = "example";
+			std::cout << "'" << arrStr2[i] << "'" << '\n';
+		}
+		std::cout << '\n';
+		size = arrStr.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << "'" << arrStr[i] << "'" << '\n';
+		}
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator in a const Array<std::string>\n\n" << RESET;
+		Array<std::string> const constArr(5);
+		size = constArr.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << "'" << constArr[i]  << "'" << '\n';
+		}
+
+		std::cout << '\n';
+		std::cout << GREEN << "trying bad index in Array<std::string>\n\n" << RESET;
+		try {
+			size = arrStr.size();
+			arrStr[size];
+		} catch (std::exception &e) {
+			std::cout << RED << "Bad index\n" << RESET;
+		}
+	}
+	{
+		unsigned int size;
+		std::cout << BLUE << "nc (complex data type)\n\n";
+		std::cout << GREEN << "Creating 2 Array<nc> (Empty & size = 5)\n\n" << RESET;
+		Array<nc> arrNc; 
+		Array<nc> arrNc2(5);
+		std::cout << "size = " << arrNc.size() << '\n';
+		std::cout << "size = " << arrNc2.size() << '\n';
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator to change and print array elements\n\n" << RESET;
+		arrNc = arrNc2;
+		arrNc2 = arrNc;
+		size = arrNc2.size();
+		for (unsigned int i = 0; i < size; i++) {
+			arrNc2[i].x = i;
+			arrNc2[i].i = size - i;
+			std::cout << arrNc2[i].x << " " << arrNc2[i].i << '\n';
+		}
+		std::cout << '\n';
+		size = arrNc.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << arrNc[i].x << " " << arrNc[i].i << '\n';
+		}
+		std::cout << '\n';
+
+		std::cout << GREEN << "Using [] operator in a const Array<nc>\n\n" << RESET;
+		Array<nc> const constArr(5);
+		size = constArr.size();
+		for (unsigned int i = 0; i < size; i++) {
+			std::cout << constArr[i].x << " " << constArr[i].i << '\n';
+		}
+
+		std::cout << '\n';
+		std::cout << GREEN << "trying bad index in Array<nc>\n\n" << RESET;
+		try {
+			size = arrNc.size();
+			arrNc[size];
+		} catch (std::exception &e) {
+			std::cout << RED << "Bad index\n" << RESET;
+		}
+	}
+	return 0;
+}

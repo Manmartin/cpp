0670fe0db05821dca49e74210172b865a4238b63
Manmartin <manuelmartinlopez02@gmail.com>
2023-03-31 20:05:10 +0200

    Cpp09 ex01 finished

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -1,13 +1,18 @@
 #include "RPN.hpp"
 
+
+/* Constructor and Destructor */
 RPN::RPN() {}
 RPN::RPN( RPN const &ref ) { *this = ref; }
 RPN::~RPN() {}
+
+/* Operators*/
 
 RPN &RPN::operator=( RPN const &ref ) {
     this->_stack = ref._stack;
     return *this;
 }
+/* Reader */
 
 void RPN::getNumbers(float *n1, float *n2) {
     *n1 = _stack.top();
@@ -20,44 +25,46 @@ bool RPN::readExpression( std::string exp ) {
     float first, second;
 
     for (unsigned i = 0; exp[i] != '\0'; i++) {
-        if (isdigit(exp[i])) {
-            if (exp[i+1] != ' ' && exp[i+1] != '\0')
-                return false;
+        if (exp[i] == ' ')
+            continue ;
+        else if (exp[i + 1] != ' ' && exp[i + 1] != '\0')
+            return false;
+        else if (isdigit(exp[i]))
             _stack.push(exp[i] - '0');
-            continue; 
-        }
-        else if (exp[i] == ' ')
-            continue;
-        switch (exp[i])
-        {
-            case '+':
-                if (_stack.size() < 2)
+        else {
+            switch (exp[i])
+            {
+                case '+':
+                    if (_stack.size() < 2)
+                        return false;
+                    getNumbers(&second, &first);
+                    _stack.push(first + second);
+                    break;
+                case '-':
+                    if (_stack.size() < 2)
+                        return false;
+                    getNumbers(&second, &first);
+                    _stack.push(first - second);
+                    break;
+                case '*':
+                    if (_stack.size() < 2)
+                        return false;
+                    getNumbers(&second, &first);
+                    _stack.push(first * second);
+                    break;
+                case '/':
+                    if (_stack.size() < 2)
+                        return false;
+                    getNumbers(&second, &first);
+                    _stack.push(first / second);
+                    break;
+                default:
                     return false;
-                getNumbers(&second, &first);
-                _stack.push(first + second);
-                break;
-            case '-':
-                if (_stack.size() < 2)
-                    return false;
-                getNumbers(&second, &first);
-                _stack.push(first - second);
-                break;
-            case '*':
-                if (_stack.size() < 2)
-                    return false;
-                getNumbers(&second, &first);
-                _stack.push(first * second);
-                break;
-            case '/':
-                if (_stack.size() < 2)
-                    return false;
-                getNumbers(&second, &first);
-                _stack.push(first / second);
-                break;
-            default:
-                return false;
+            }
         }
     }
+    if ( _stack.size() != 1 )
+        return false;
     std::cout << _stack.top() << '\n';
     return true;
 }
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -7,16 +7,15 @@
 class RPN
 {
     private:
+        RPN( RPN const &ref );
+        RPN &operator=( RPN const &ref );
+
         std::stack<float>     _stack;
 
-        void   getNumbers( float *n1, float *n2);
-
+        void   getNumbers( float *n1, float *n2 );
     public:
         RPN();
-        RPN( RPN const &ref );
         ~RPN();
-
-        RPN &operator=( RPN const &ref );
 
         bool    readExpression( std::string exp );
 };
